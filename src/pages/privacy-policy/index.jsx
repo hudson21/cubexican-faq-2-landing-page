35a312c503a195796961f38cbb81777c989f0f49
hudson21 <camihudsondiaz@ittepic.edu.mx>
2022-09-28 20:58:33 -0500

    Finished with Video Page

--- src/pages/privacy-policy/index.jsx
+++ src/pages/privacy-policy/index.jsx
@@ -10,51 +10,7 @@ export default function PrivacyPolicyPage() {
               <h2 className="sec-title-lg" style={{ color: '#6480ce' }}>
                 Privacy Policy
               </h2>
-
               <Privacy />
-              {/* <p className="sec-text-pad lead" style={{ textAlign: 'justify' }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-
-              <p className="sec-text-pad lead" style={{ textAlign: 'justify' }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-
-              <p className="sec-text-pad lead" style={{ textAlign: 'justify' }}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p> */}
             </div>
           </div>
         </div>
--- src/pages/video-documentation/index.jsx
+++ src/pages/video-documentation/index.jsx
@@ -1,3 +1,29 @@
+import './index.css';
+
 export default function VideoDocumentationPage() {
-  return <>I am the Video Documentation Page</>;
+  return (
+    <div className="popular-pricing section-pad-lg section-pad nopb">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-12 text-center">
+            <div className="sec-heading">
+              <h2 className="sec-title-lg" style={{ color: '#6480ce' }}>
+                Video Documentation
+              </h2>
+
+              <div className="video-container">
+                <iframe
+                  src="https://www.youtube.com/embed/Zccj5yf4rJA"
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; encrypted-media; gyroscope;"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
